91b2adc70f2707f71b4096037b80c172aeb09e70
Gonzalo Caminos <gonzalogabrielcaminos@gmail.com>
2022-01-11 18:29:06 -0300

    tratando de subir a produccion

--- index.js
+++ index.js
@@ -14,5 +14,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/', routes());
+//modo dev
 
-app.listen(5000);
+//app.listen(5000);
+//modo production
+
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 5000;
+//iniciar app
+app.listen(port,host,()=>{
+    console.log(`Server is running on port ${port}`);
+});
